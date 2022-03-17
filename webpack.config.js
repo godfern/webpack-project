69b023076213504e1fb95db992755c738cfd46fd
Godfrey <godfrey.f@flipkart.com>
2022-03-17 17:16:31 +0530

    Added scss loader, browserlist, hot loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 let mode = "development";
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     mode = "production";
 }
 
@@ -8,17 +9,28 @@ module.exports = {
     mode: mode,
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
+                test: /\.s?css$/i,
+                use: [MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
             }
-        }]
+        ]
     },
+    plugins: [new MiniCssExtractPlugin()],
     devtool: "inline-source-map",
     devServer: {
         static: {
             directory: "./dist"
-        }
+        },
+        hot: true
     }
 }
